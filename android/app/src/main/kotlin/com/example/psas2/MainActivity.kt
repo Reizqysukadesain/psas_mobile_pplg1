3f989387c6df691b5e329101fccdba49e1f48125
reizqysukadesain <reizqymarhas@gmail.com>
2024-11-26 11:26:26 +0700

    commit pertama

---
+++ android/app/src/main/kotlin/com/example/psas2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.psas2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static String tag = 'home-page';
+
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final alucard = const Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: CircleAvatar(
+          radius: 72.0,
+          backgroundColor: Colors.transparent,
+          backgroundImage: AssetImage('assets/alucard.png'),
+        ),
+      ),
+    );
+
+    final welcome = const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        'Welcome Alucard',
+        style: TextStyle(fontSize: 28.0, color: Colors.white),
+      ),
+    );
+
+    final lorem = const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        'Selamat datang baginda maharaja Alucard, kami sudah menunggu anda',
+        style: TextStyle(fontSize: 16.0, color: Colors.white),
+      ),
+    );
+
+    final body = Container(
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.all(28.0),
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 99, 18, 165),
+          Color.fromARGB(255, 91, 93, 94),
+        ]),
+      ),
+      child: Column(
+        children: <Widget>[alucard, welcome, lorem],
+      ),
+    );
+
+    return Scaffold(
+      body: body,
+    );
+  }
+}
